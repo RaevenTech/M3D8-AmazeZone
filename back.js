be2aaa681e5ef148ac0b942aa201de6a40f5a15a
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-04-14 10:44:14 +0100

    updated still has bugs

--- back.js
+++ back.js
@@ -1,38 +1,33 @@
-const getProducts = () => {
-    let product = {
-        name: document.getElementById("product-name").value,
-        description: document.getElementById("product-description").value,
-        brand: document.getElementById("product-brand").value,
-        imageUrl: document.getElementById("product-image").value,
-        price: document.getElementById("product-price").value,
+const formSubmit = async (event) => {
+    console.log(event.target);
+    event.preventDefault();
+
+    const newFormEvent = {
+        name: document.getElementById("name").value,
+        description: document.getElementById("description").value,
+        brand: document.getElementById("brand").value,
+        imageUrl: document.getElementById("image").value,
+        price: document.getElementById("price").value,
     };
-    let productsToString = JSON.stringify(product);
-    return productsToString;
-};
-console.log(getProducts());
 
-const insertNewProduct = async (err) => {
-    try {
-        err.preventDefault();
-        let product = getProducts();
-        let apiResponce = await fetch(
-            "https://striveschool-api.herokuapp.com/api/product/",
-            {
-                method: "POST",
-                body: product,
-                headers: {
-                    Authorization:
-                        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YzZmYmE5MDIzOTAwMTVkOTY2MTAiLCJpYXQiOjE2NDk4NTQyMDMsImV4cCI6MTY1MTA2MzgwM30.YiCJ31GpRLOUvO5vR5gDHAq_jwBdL_IGCSUocgHn4dE",
-                    "Content-Type": "application/json",
-                },
-            }
-        );
+    const response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/product/",
+        {
+            method: "POST",
+            body: JSON.stringify(newFormEvent),
+            headers: {
+                "Content-Type": "application/json",
+                Authorization:
+                    "Bearerey JhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YzZmYmE5MDIzOTAwMTVkOTY2MTAiLCJpYXQiOjE2NDk4NTQyMDMsImV4cCI6MTY1MTA2MzgwM30.YiCJ31GpRLOUvO5vR5gDHAq_jwBdL_IGCSUocgHn4dE",
+            },
+        }
+    );
 
-        let apiData = await apiResponce.json();
-        console.log(apiData);
-        document.querySelector("form").reset();
-        alert(`${apiData.name} has been added`);
-    } catch (err) {
-        alert(error);
+    if (response.ok) {
+        const body = await response.json();
+
+        alert("New item added in store - id: " + body._id);
+        console.log(response);
     }
+    window.location.reload();
 };
--- front.js
+++ front.js
@@ -1,69 +1,90 @@
-async function getProducts() {
-    try {
-        let productsJSON = await fetch(
-            "https://striveschool-api.herokuapp.com/api/product/",
-            {
-                headers: {
-                    Authorization:
-                        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YzZmYmE5MDIzOTAwMTVkOTY2MTAiLCJpYXQiOjE2NDk4NTQyMDMsImV4cCI6MTY1MTA2MzgwM30.YiCJ31GpRLOUvO5vR5gDHAq_jwBdL_IGCSUocgHn4dE",
-                },
-            }
-        );
-        let productsList = await productsJSON.json();
-        productsList.shift();
-
-        let brandList = getBrands(productsList);
-        newSection(brandList);
-        appendCards(productsList);
-    } catch (error) {
-        console.log(error);
-        alert(error);
-    }
-}
-
-window.omlaod = () => {
-    getProducts();
+const options = {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json",
+        Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YmNiZmE5MDIzOTAwMTVkOTY1YzYiLCJpYXQiOjE2NDk4NTE1ODMsImV4cCI6MTY1MTA2MTE4M30.1jc7LN9eMX4yZ7HWl4wazVJ2SyiKUzAHtFm0IglDnhY",
+    },
 };
 
-const newSection = (brands) => {
-    let hero = document.querySelector(".jumbotron.hero");
-    brands.forEach((brand) => {
-        let newSection = document.createElement("section");
-        newSection.classList.add("container", "brandSection", "px-0");
+let firstfucntionPageinfo = async () => {
+    const response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/product/",
+        options
+    );
+    const parsedBody = await response.json();
+    console.log(parsedBody);
 
-        let newSectionHeading = document.createElement("h2");
-        newSectionHeading.classList.add("row", "mx-0");
-        newSectionHeading.innerText = brand;
-        newSection.appendChild(newSectionHeading);
+    let listgroup = document.querySelector(".list-group");
 
-        let productCardsDisplay = document.createElement("div");
-        productCardsDisplay.classList.add("row", "insertProduct");
-        productCardsDisplay.id = brand.toLowerCase();
-    });
-};
-
-function getBrands(data) {
-    let brands = [];
-    data.filter((product) => {
-        if (brands.includes(product.brand) === false)
-            brands.push(product.brand);
-    });
-    return brands;
-}
-
-function generateCards(product) {
-    return `
-        <div class="card mb-4 col-3">
-            <div class="row">
-                <a>
-                    <img id="main-page-img-url" src="${product.imageUrl}" class="card-img-top img-fluid" alt="...">
-                    <span class="badge badge-warning">£ ${product.price}</span>
-                </a>
-                <div class="card-body p-0 mt-3">
-                        <h5 class="card-title text-truncate">Name: ${product.name}</h5>
-                        <p class="card-text mb-3">Description:  ${product.description}</p>
+    listgroup.innerHTML = "";
+    parsedBody.forEach((userinput) => {
+        let li1 = document.createElement("li");
+        li1.innerHTML = `
+      <li class="list-group-item d-flex justify-content-between align-items-center"
+              >
+                <div>
+                  <h2 class="brand-name">${userinput.name}</h2>
+                  <p>Brand: ${userinput.brand}</p>
+                  <p>Description: ${userinput.description}</p>
+                  <p><a href="${userinput.imageUrl}">Image</a></p>
+                  <small><p class="unique-id">${userinput._id}</p></small>
+                  
+                  
                 </div>
-            </div>
-        </div>
-    `;
-}
+               <div><span class="badge badge-secondary badge-pill">€${userinput.price}</span></div>
+            
+              </li>`;
+        listgroup.appendChild(li1);
+    });
+};
+
+window.onload = () => {
+    firstfucntionPageinfo();
+};
+
+/* THE DELETE BUTTON FUNCTION 
+
+const options2 = {
+    method: "DELETE",
+    headers: {
+        "Content-Type": "application/json",
+        Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YmNiZmE5MDIzOTAwMTVkOTY1YzYiLCJpYXQiOjE2NDk4NTE1ODMsImV4cCI6MTY1MTA2MTE4M30.1jc7LN9eMX4yZ7HWl4wazVJ2SyiKUzAHtFm0IglDnhY",
+    },
+};
+
+let deleteButtonFunction = async (event) => {
+    console.log(
+        event.target.parentElement.parentElement.querySelector(".unique-id")
+            .innerText
+    );
+    let uniqueId =
+        event.target.parentElement.parentElement.querySelector(
+            ".unique-id"
+        ).innerText;
+    let uniqueProductName =
+        event.target.parentElement.parentElement.querySelector(
+            ".unique-name"
+        ).innerText;
+
+    const response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/product/" + uniqueId,
+        options2
+    );
+    const parsedBody = await response.json();
+    console.log(parsedBody);
+
+    alert(
+        uniqueProductName +
+            " has now been deleted. " +
+            "This was the uniqueID: " +
+            uniqueId
+    );
+    window.location.reload();
+
+     <div>
+                <span class="badge badge-primary badge-pill">$${userinput.price}</span>
+                <i class="bi bi-trash3-fill delete-button" onclick="deleteButtonFunction(event)"></i>
+                </div>
+};*/
